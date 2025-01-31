b29b1022f165dbad989f726b196a55c3d66cc990
nikachitava <chitava18.official@gmail.com>
2025-01-31 15:59:14 +0400

    setup for components for form

--- components/CustomDropDown.tsx
+++ components/CustomDropDown.tsx
@@ -1,70 +1,85 @@
 import { View, Text, TouchableOpacity, ScrollView } from "react-native";
 import React, { useState } from "react";
+import { Control, Controller } from "react-hook-form";
 
 interface CustomDropDownProps {
+	name: string;
+	control: Control<any>;
 	data: string[];
 	placehoder: string;
 }
 
 const CustomDropDown: React.FC<CustomDropDownProps> = ({
+	name,
+	control,
 	data,
 	placehoder,
 }) => {
-	const [value, setValue] = useState("");
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
 
 	const toggleDropDownMenu = () => {
-		setIsMenuOpen((isMenuOpen) => !isMenuOpen);
-	};
-
-	const selectOption = (option: string) => {
-		setValue(option);
-		setIsMenuOpen(false);
+		setIsMenuOpen((prev) => !prev);
 	};
 
 	return (
-		<View className="relative">
-			<TouchableOpacity
-				className="p-2 border border-black rounded-lg"
-				onPress={toggleDropDownMenu}
-				activeOpacity={0.7}
-			>
-				<Text className="font-rubik-medium text-base text-text">
-					{value ? value : placehoder}
-				</Text>
-			</TouchableOpacity>
-			{isMenuOpen && (
-				<View className="absolute top-[100%] left-0 right-0 mt-1 border border-gray-300 rounded-lg bg-white z-50 shadow-lg">
-					<ScrollView
-						className="p-2 "
-						showsVerticalScrollIndicator={false}
-						style={{ maxHeight: 150 }}
+		<Controller
+			name={name}
+			control={control}
+			render={({ field: { onChange, value }, fieldState: { error } }) => (
+				<View className="relative">
+					<TouchableOpacity
+						className="p-2 border border-black rounded-lg"
+						onPress={toggleDropDownMenu}
+						activeOpacity={0.7}
 					>
-						{data.map((item, index) => (
-							<TouchableOpacity
-								key={index}
-								onPress={() => selectOption(item)}
-								className={`p-3 ${
-									index < data.length - 1
-										? "border-b border-gray-200"
-										: ""
-								}`}
+						<Text className="font-rubik-medium text-base text-text">
+							{value ? value : placehoder}
+						</Text>
+					</TouchableOpacity>
+
+					{isMenuOpen && (
+						<View className="absolute top-[100%] left-0 right-0 mt-1 border border-gray-300 rounded-lg bg-white z-50 shadow-lg">
+							<ScrollView
+								className="p-2"
+								showsVerticalScrollIndicator={false}
+								style={{ maxHeight: 150 }}
 							>
-								<Text
-									className={`font-rubik-regular text-base ${
-										value === item
-											? "text-primary font-rubik-semibold"
-											: "text-text"
-									}`}
-								>
-									{item}
-								</Text>
-							</TouchableOpacity>
-						))}
-					</ScrollView>
+								{data.map((item, index) => (
+									<TouchableOpacity
+										key={index}
+										onPress={() => {
+											onChange(item);
+											setIsMenuOpen(false);
+										}}
+										className={`p-3 ${
+											index < data.length - 1
+												? "border-b border-gray-200"
+												: ""
+										}`}
+									>
+										<Text
+											className={`font-rubik-regular text-base ${
+												value === item
+													? "text-primary font-rubik-semibold"
+													: "text-text"
+											}`}
+										>
+											{item}
+										</Text>
+									</TouchableOpacity>
+								))}
+							</ScrollView>
+						</View>
+					)}
+
+					{error && (
+						<Text className="text-red-500 text-sm mt-1">
+							{error.message}
+						</Text>
+					)}
 				</View>
 			)}
-		</View>
+		/>
 	);
 };
 
--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,17 +1,28 @@
 import { TextInput, View } from "react-native";
+import { Control, Controller } from "react-hook-form";
 
 interface CustomInputPropsTypes {
+	control: Control<any>;
 	name: string;
 }
 
-const CustomInput = () => {
+const CustomInput: React.FC<CustomInputPropsTypes> = ({ name, control }) => {
 	return (
-		<View>
-			<TextInput
-				placeholder="Enter player name"
-				className="h-12 px-4 border border-black p-2 rounded-lg"
-			/>
-		</View>
+		<Controller
+			name={name}
+			control={control}
+			render={({ field: { onChange, value, onBlur } }) => (
+				<View>
+					<TextInput
+						placeholder="Enter player name"
+						className="h-12 px-4 border border-black p-2 rounded-lg font-rubik-regular"
+						onChangeText={onChange}
+						value={value}
+						onBlur={onBlur}
+					/>
+				</View>
+			)}
+		/>
 	);
 };
 
