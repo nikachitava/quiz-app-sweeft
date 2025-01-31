ee86b4ff5569b22dcf9726be0df9ab8a33a2523a
nikachitava <chitava18.official@gmail.com>
2025-01-31 13:08:37 +0400

    load fonts to app

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
+import CustomInput from "@/components/CustomInput";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
 	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: "center",
-				alignItems: "center",
-			}}
-		>
-			<Text>Edit app/index.tsx to edit this screen.</Text>
-		</View>
+		<SafeAreaView className="flex-1 justify-center bg-background">
+			<View className="p-5">
+				<Text className="text-primary text-center font-bold py-4 text-2xl font-rubik-extrabold">
+					Quizz App
+				</Text>
+
+				<CustomInput />
+			</View>
+		</SafeAreaView>
 	);
 }
