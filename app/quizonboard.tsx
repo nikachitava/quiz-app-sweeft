980dd8942c759ee217eb22065dfca232c0f9cfdd
nikachitava <chitava18.official@gmail.com>
2025-01-31 21:25:49 +0400

    working on quizonboard screen

--- app/quizonboard.tsx
+++ app/quizonboard.tsx
@@ -1,16 +1,28 @@
-import { View, Text } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import { View, Text, ScrollView } from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Countdown from "@/components/ui/Countdown";
+import GameInfo from "@/components/ui/GameInfo";
 
 const quizonboard = () => {
 	const params = useLocalSearchParams();
-	const { playerName, category, difficulty } = params;
+	const { category, difficulty } = params;
 	return (
-		<View>
-			<Text>PlayerName: {playerName} </Text>
-			<Text>Category: {category}</Text>
-			<Text>Difficulty: {difficulty}</Text>
-		</View>
+		<SafeAreaView className="h-full">
+			<ScrollView className="h-full flex-1 justify-center">
+				<View className="font-rubik-semibold uppercase text-primary text-4xl text-center">
+					Get Ready
+				</View>
+				<View className="text-center font-rubik-medium text-text">
+					<Countdown
+						initialCount={500}
+						onCountdownComplete={() => router.push("/quiz")}
+					/>
+				</View>
+				<GameInfo category={category} difficulty={difficulty} />
+			</ScrollView>
+		</SafeAreaView>
 	);
 };
 
