d8c929a18fcae0025eafc9ba6231556329ee94bb
nikachitava <chitava18.official@gmail.com>
2025-01-31 20:27:51 +0400

    get data from query

--- components/forms/QuizSetupForm.tsx
+++ components/forms/QuizSetupForm.tsx
@@ -1,15 +1,14 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CustomInput from "../ui/CustomInput";
 import CustomDropDown from "../ui/CustomDropDown";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { router } from "expo-router";
 import { quizSetupFormSchema } from "@/schemes/quizSetupFormSchema";
+import { useFetchCategoriesQuery } from "@/services/api/categoriesQueries";
 
 const QuizSetupForm = () => {
-	const DATA = ["SPORT", "GEOGRAPHY", "SCIENSE", "DEVELOPMENT", "LANGUAGES"];
-
 	const { control, handleSubmit } = useForm({
 		resolver: zodResolver(quizSetupFormSchema),
 	});
@@ -18,17 +17,19 @@ const QuizSetupForm = () => {
 		router.navigate({ pathname: "/quizonboard", params: data });
 	};
 
+	const { data } = useFetchCategoriesQuery();
+
 	return (
 		<View className="gap-4">
 			<CustomInput name="playerName" control={control} />
 			<CustomDropDown
-				data={DATA}
+				data={data?.trivia_categories}
 				placeholder="Select category"
 				name="category"
 				control={control}
 			/>
 			<CustomDropDown
-				data={DATA}
+				data={data?.trivia_categories}
 				placeholder="Select difficulty"
 				name="difficulty"
 				control={control}
