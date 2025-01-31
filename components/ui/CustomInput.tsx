c6333b357581f18f25b49d085d76ce56a2937ed9
nikachitava <chitava18.official@gmail.com>
2025-01-31 19:20:27 +0400

    add error fieldstate in custominput component

--- components/ui/CustomInput.tsx
+++ components/ui/CustomInput.tsx
@@ -1,4 +1,4 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, Text } from "react-native";
 import { Controller } from "react-hook-form";
 import { CustomInputPropsTypes } from "@/types/CustomInputPropsTypes";
 
@@ -7,8 +7,11 @@ const CustomInput: React.FC<CustomInputPropsTypes> = ({ name, control }) => {
 		<Controller
 			name={name}
 			control={control}
-			render={({ field: { onChange, value, onBlur } }) => (
-				<View>
+			render={({
+				field: { onChange, value, onBlur },
+				fieldState: { error },
+			}) => (
+				<View className="gap-2">
 					<TextInput
 						placeholder="Enter player name"
 						className="h-12 px-4 border border-black p-2 rounded-lg font-rubik-regular"
@@ -16,6 +19,11 @@ const CustomInput: React.FC<CustomInputPropsTypes> = ({ name, control }) => {
 						value={value}
 						onBlur={onBlur}
 					/>
+					{error && (
+						<Text className="text-sm font-rubik-medium text-error">
+							{error.message}
+						</Text>
+					)}
 				</View>
 			)}
 		/>
