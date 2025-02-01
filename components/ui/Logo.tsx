56ac98e16cdff4924d4c1493ac718855d033ba34
nikachitava <chitava18.official@gmail.com>
2025-02-01 21:37:59 +0400

    Create logo component

---
+++ components/ui/Logo.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import React from "react";
+
+const Logo = () => {
+	return (
+		<Text className="text-primary text-center py-4 text-4xl font-rubik-extrabold">
+			Quizz App
+		</Text>
+	);
+};
+
+export default Logo;
