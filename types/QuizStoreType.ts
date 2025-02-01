08c6169e92f09fb0217ebfdf4a3730720fd395f8
nikachitava <chitava18.official@gmail.com>
2025-02-01 23:17:28 +0400

    add neccesery types

--- types/QuizStoreType.ts
+++ types/QuizStoreType.ts
@@ -2,7 +2,9 @@ import { TriviaQuestion } from "./QuestionsReponse";
 
 export type QuizStoreType = {
     points: number;
+    incrementPoint: () => void;
     currentQuestion: number;
+    nextQuestion: () => void;
     questionsList: TriviaQuestion[],
     setQuestionList: (category: number | string , difficulty: number | string) => Promise<void>;
 }
