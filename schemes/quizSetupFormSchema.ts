f09ef84da4e3d54ffc0d2f45bd0eb743a4c9b59f
nikachitava <chitava18.official@gmail.com>
2025-02-01 18:34:09 +0400

    change category type and playername min character number

--- schemes/quizSetupFormSchema.ts
+++ schemes/quizSetupFormSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const quizSetupFormSchema = z.object({
-    playerName: z.string().min(6, "Name must be at least 6 characters"),
-    category: z.string().min(1, "You must select category"),
+    playerName: z.string().min(2, "Name must be at least 2 characters"),
+    category: z.number().min(0, "Category is required"),
     difficulty: z.string().min(1, "You must select difficulty"),
 });
