3740333bcad2c081a1e89c3c14ee2227306c7d26
nikachitava <chitava18.official@gmail.com>
2025-02-01 17:10:59 +0400

    working on quiz screen to implement the game

--- app/quiz.tsx
+++ app/quiz.tsx
@@ -1,10 +1,37 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView } from "react-native";
+import React, { useEffect } from "react";
+import { useQuizStore } from "@/states/quizStore";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function quiz() {
+	const { currentQuestion, points, questionsList, setQuestionList } =
+		useQuizStore((state) => state);
+
+	useEffect(() => {
+		setQuestionList(24, "easy");
+	}, []);
+
+	console.log("questionsList", questionsList);
+
 	return (
-		<View>
-			<Text>quiz</Text>
-		</View>
+		<SafeAreaView className="h-full">
+			<ScrollView className="h-full flex-1">
+				<Text>Quizsz</Text>
+				<View>
+					<View>
+						<Text>{questionsList[currentQuestion].category}</Text>
+						<Text>
+							{questionsList[currentQuestion].correct_answer}
+						</Text>
+						<Text>{questionsList[currentQuestion].difficulty}</Text>
+						<Text>
+							{questionsList[currentQuestion].incorrect_answers}
+						</Text>
+						<Text>{questionsList[currentQuestion].question}</Text>
+						<Text>{questionsList[currentQuestion].type}</Text>
+					</View>
+				</View>
+			</ScrollView>
+		</SafeAreaView>
 	);
 }
