6e25417106eb90d50f8c25e2a2b7f4a1553220f7
nikachitava <chitava18.official@gmail.com>
2025-02-01 23:18:29 +0400

    implement question displayer on quiz screen

--- app/quiz.tsx
+++ app/quiz.tsx
@@ -1,41 +1,15 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
 import React from "react";
-import { useQuizStore } from "@/states/quizStore";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Logo from "@/components/ui/Logo";
+import QuestionDisplay from "@/components/ui/QuestionDisplay";
 
 export default function quiz() {
-	const { currentQuestion, points, questionsList, setQuestionList } =
-		useQuizStore((state) => state);
-
 	return (
 		<SafeAreaView className="h-full">
 			<ScrollView className="h-full flex-1">
-				<Text>Quizsz</Text>
-				{questionsList && (
-					<View>
-						<View>
-							<Text>
-								{questionsList[currentQuestion].category}
-							</Text>
-							<Text>
-								{questionsList[currentQuestion].correct_answer}
-							</Text>
-							<Text>
-								{questionsList[currentQuestion].difficulty}
-							</Text>
-							<Text>
-								{
-									questionsList[currentQuestion]
-										.incorrect_answers
-								}
-							</Text>
-							<Text>
-								{questionsList[currentQuestion].question}
-							</Text>
-							<Text>{questionsList[currentQuestion].type}</Text>
-						</View>
-					</View>
-				)}
+				<Logo />
+				<QuestionDisplay />
 			</ScrollView>
 		</SafeAreaView>
 	);
