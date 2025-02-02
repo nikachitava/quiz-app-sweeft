94ecc9f007acae559b3b266b94fdec7619db61ca
nikachitava <chitava18.official@gmail.com>
2025-02-02 16:37:13 +0400

    improve the code and make more modern UI

--- components/ui/GameInfo.tsx
+++ components/ui/GameInfo.tsx
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 import React from "react";
 import { GameInfoProps } from "@/types/GameInfoProps";
 import { useFetchCategoriesQuery } from "@/services/api/categoriesQueries";
+import { InfoCard } from "./InfoCard";
 
 const GameInfo: React.FC<GameInfoProps> = ({ category, difficulty }) => {
 	const { data } = useFetchCategoriesQuery();
@@ -15,23 +16,52 @@ const GameInfo: React.FC<GameInfoProps> = ({ category, difficulty }) => {
 
 	const categoryName = getCategoryName(Number(category));
 
+	const getDifficultyColor = (difficulty: string) => {
+		switch (difficulty.toLowerCase()) {
+			case "easy":
+				return "text-green-500";
+			case "medium":
+				return "text-yellow-500";
+			case "hard":
+				return "text-red-500";
+			default:
+				return "text-gray-500";
+		}
+	};
+
+	const getDifficultyIcon = (difficulty: string) => {
+		switch (difficulty.toLowerCase()) {
+			case "easy":
+				return "star";
+			case "medium":
+				return "staro";
+			case "hard":
+				return "warning";
+			default:
+				return "questioncircleo";
+		}
+	};
+
 	return (
-		<View className="mt-10 justify-between items-center px-20 gap-2">
-			<View className="flex-row">
-				<Text className="mr-2 font-rubik-medium text-text text-lg">
-					Questions Category:
-				</Text>
-				<Text className="font-rubik-medium text-text text-lg">
-					{categoryName?.name}
-				</Text>
-			</View>
-			<View className="flex-row ">
-				<Text className="mr-2 font-rubik-medium text-text text-lg">
-					Difficulty:
-				</Text>
-				<Text className="font-rubik-medium text-text text-lg">
-					{difficulty}
-				</Text>
+		<View className="bg-white/10 rounded-2xl p-6  backdrop-blur-lg w-full">
+			<View className="space-y-4">
+				<InfoCard
+					icon="folder1"
+					label="Category"
+					value={categoryName?.name || ""}
+				/>
+
+				<InfoCard
+					icon={getDifficultyIcon(difficulty.toString())}
+					label="Difficulty"
+					value={
+						difficulty.toString().charAt(0).toUpperCase() +
+						difficulty.toString().slice(1)
+					}
+					valueColor={getDifficultyColor(difficulty.toString())}
+				/>
+
+				<InfoCard icon="questioncircleo" label="Questions" value="10" />
 			</View>
 		</View>
 	);
